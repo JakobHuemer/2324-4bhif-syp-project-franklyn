d6adb93f7b21b7e16ca852634c4b41750ea519bd
HikariTempest <arwed@gmx.at>
2024-10-27 14:27:16 +0100

    feat: implemented the basic layout for the create-exam route and generated a create-exam component
    #244bfk-165 In Progress

--- instructor-client/src/app/components/router-components/create-test/create-test.component.ts
+++ instructor-client/src/app/components/router-components/create-test/create-test.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CreateExamComponent} from "../../entity-components/create-exam/create-exam.component";
 
 @Component({
   selector: 'app-create-test',
   standalone: true,
-  imports: [],
+  imports: [
+    CreateExamComponent
+  ],
   templateUrl: './create-test.component.html',
   styleUrl: './create-test.component.css'
 })
