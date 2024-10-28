8e5b2c30822d6b81a7236e8e6a3db76180976e80
HikariTempest <arwed@gmx.at>
2024-10-28 10:34:46 +0100

    feat: removed unnecessary value from dto and added environment variables for the school units
    #244bfk-165 In Progress

--- instructor-client/env/environment.production.ts
+++ instructor-client/env/environment.production.ts
@@ -12,5 +12,76 @@ export const environment = {
   reloadDashboardInterval: 5,
   metricsDashboardValueOkay: "#03c303",
   metricsDashboardValueBarelyOkay: "#e6e604",
-  metricsDashboardValueNotOkay: "#cf0404"
+  metricsDashboardValueNotOkay: "#cf0404",
+  schoolUnitMinutes: 50,
+  eveningSchoolUnitMinutes: 45,
+  displayEveningSchoolUnits: true,
+  schoolUnits: [
+    {
+      id: 1,
+      time: "08:00 AM"
+    },
+    {
+      id: 2,
+      time: "08:55 AM"
+    },
+    {
+      id: 3,
+      time: "10:00 AM"
+    },
+    {
+      id: 4,
+      time: "10:55 AM"
+    },
+    {
+      id: 5,
+      time: "11:50 AM"
+    },
+    {
+      id: 6,
+      time: "12:45 PM"
+    },
+    {
+      id: 7,
+      time: "13:40 PM"
+    },
+    {
+      id: 8,
+      time: "14:35 PM"
+    },
+    {
+      id: 9,
+      time: "15:30 PM"
+    },
+    {
+      id: 10,
+      time: "16:25 PM"
+    }
+  ],
+  eveningSchoolUnits: [
+    {
+      id: 11,
+      time: "17:20 PM"
+    },
+    {
+      id: 12,
+      time: "18:05 PM"
+    },
+    {
+      id: 13,
+      time: "19:00 PM"
+    },
+    {
+      id: 14,
+      time: "19:45 PM"
+    },
+    {
+      id: 15,
+      time: "20:40 PM"
+    },
+    {
+      id: 16,
+      time: "21:25 PM"
+    },
+  ]
 }
--- instructor-client/src/app/model/entity/dto/CreateExamDto.ts
+++ instructor-client/src/app/model/entity/dto/CreateExamDto.ts
@@ -2,6 +2,5 @@ export interface CreateExamDto {
   title: string
   start: Date,
   end: Date,
-  screencapture_interval_seconds: number,
-  startBeforeEnd: boolean
+  screencapture_interval_seconds: number
 }
