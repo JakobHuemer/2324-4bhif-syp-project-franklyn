d7d4af469168f2054ed422dbc814ff4fb513b014
HikariTempest <arwed@gmx.at>
2025-01-21 15:01:25 +0100

    refactor: removed job-logic in the video examinee again, since this won't be needed anymore
    #244bfk-194

--- instructor-client/src/app/components/entity-components/video-examinee/video-examinee.component.ts
+++ instructor-client/src/app/components/entity-components/video-examinee/video-examinee.component.ts
@@ -1,15 +1,11 @@
 import {Component, inject, Input} from '@angular/core';
 import {StoreService} from "../../../services/store.service";
-import {Examinee, JobState} from "../../../model";
+import {Examinee} from "../../../model";
 import {environment} from "../../../../../env/environment";
-import {AsyncPipe} from "@angular/common";
-import {distinctUntilChanged, map} from "rxjs";
 
 @Component({
     selector: 'app-video-examinee',
-  imports: [
-    AsyncPipe
-  ],
+  imports: [],
     templateUrl: './video-examinee.component.html',
     styleUrl: './video-examinee.component.css'
 })
@@ -18,19 +14,6 @@ export class VideoExamineeComponent {
 
   @Input() examId: number | undefined;
   @Input() examinee: Examinee | undefined;
-  @Input() jobId: number | undefined;
-
-  protected job = this.store
-    .pipe(
-      map(model =>
-        model.jobServiceModel.jobs
-          .filter(job =>
-            !job.shouldDownload &&
-            job.state === JobState.DONE &&
-            job.id === this.jobId
-          ).at(0)),
-      distinctUntilChanged()
-    );
 
   getVideoUrl(): string {
     return `${environment.serverBaseUrl}/video/${this.examinee?.firstname}-${this.examinee?.lastname}?cache=${this.store.value.patrolModeModel.cacheBuster.cachebustNum}`; //examinee gets checked in the html
