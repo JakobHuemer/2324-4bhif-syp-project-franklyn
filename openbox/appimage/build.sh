cfa833377dbe5c6ce234d6caf4e09852d89a9578
icqun <jln.schludermann@gmail.com>
2025-04-08 14:02:22 +0200

    fix: disable interactive build

--- openbox/appimage/build.sh
+++ openbox/appimage/build.sh
@@ -7,7 +7,7 @@ _dir=$(realpath $(dirname $0))
 git submodule update --init --recursive
 
 docker build -t openbox-builder/ubuntu $_dir
-docker run -it -v $_dir/../../:/franklyn openbox-builder/ubuntu /bin/bash -c "
+docker run -v $_dir/../../:/franklyn openbox-builder/ubuntu /bin/bash -c "
 cd /franklyn/openbox/
 RUSTFLAGS='-C target-feature=-crt-static' cargo build --profile release-opt --package openbox --features $1
 "
